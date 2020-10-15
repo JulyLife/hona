4a9f8cb2d65a46432cd8ec26d4f10f0aff47051c
JulyLife-C60 <ghanima.sabti@gmail.com>
2020-10-16 00:04:15 +0300

    first commit

---
+++ Hona/Hona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hona
+//
+//  Created by ghanima on 10/16/20.
+//  Copyright © 2020 ghanima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
